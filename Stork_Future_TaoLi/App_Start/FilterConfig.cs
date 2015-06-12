8bd14bf7a34b3663924cedcaad79728b7e84bcbe
skcncspeaker@hotmail.com <skcncspeaker@hotmail.com>
2015-06-12 15:46:53 +0800

    初始创建项目

---
+++ Stork_Future_TaoLi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Stork_Future_TaoLi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Stork_Future_TaoLi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stork_Future_TaoLi.MvcApplication" Language="C#" %>
