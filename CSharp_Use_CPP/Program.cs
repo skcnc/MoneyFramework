0106a8f5ef077c009b59715b751d2271712933f6
skcncspeaker@hotmail.com <skcncspeaker@hotmail.com>
2015-06-15 15:13:04 +0800

    update 2015-06-15

---
+++ CSharp_Use_CPP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExportDLL;
+
+namespace CSharp_Use_CPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExportDLL.CLog4C c1 = new CLog4C();
+            ExportDLL.CLog4C c2 = new CLog4C();
+
+            int k1 = c1.cal(3, 4);
+            int k2 = c2.cal(3, 5);
+
+            Console.WriteLine("k1 = " + k1);
+            Console.WriteLine("k2 = " + k2);
+            String s = string.Empty;
+            
+
+            Console.ReadLine();
+        }
+    }
+}
