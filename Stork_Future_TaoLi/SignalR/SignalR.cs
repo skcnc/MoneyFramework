a5b6ce5b88f32809d9703d930b5455d21c2c1ac0
chengsun <skcncspeaker@hotmail.com>
2015-07-28 07:15:49 +0800

    update

---
+++ Stork_Future_TaoLi/SignalR/SignalR.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using Stork_Future_TaoLi;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(Stork_Future_TaoLi.Startup))]
+namespace Stork_Future_TaoLi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app) { app.MapSignalR(); }
+    }
+
+    public class ChatHub : Hub
+    {
+        public void Send(String name, String message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+
+
+}
