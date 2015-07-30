4ab62c21d2c6cf8f30c34ed0bb783c497249c5bc
cheng <skcncspeaker@hotmail.com>
2015-07-30 10:28:15 +0800

    update

---
+++ Stork_Future_TaoLi/MarketInfo/UpdateMarketPanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace Stork_Future_TaoLi.MarketInfo
+{
+    public class UpdateMarketPanel
+    {
+        private void Run()
+        {
+            Thread excutedThread = new Thread(new ThreadStart(ThreadProc));
+            excutedThread.Start();
+        }
+
+        private void ThreadProc()
+        {
+
+        }
+    }
+}
---
+++ Stork_Future_TaoLi/Queues/marketMonitorQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stork_Future_TaoLi
+{
+    public class MarketInfo
+    {
+        public String Code { get; set; }
+        public String Value { get; set; }
+    }
+
+    public class marketMonitorQueue
+    {
+        private static Queue instance;
+
+        public static void EnQueueNew(String code, String value)
+        {
+            instance.Enqueue((object)(new MarketInfo()
+            {
+                Code = code,
+                Value = value
+            }));
+        }
+
+        public static MarketInfo DeQueueNew()
+        {
+            if (instance.Count == 0) return null;
+            return (MarketInfo)(instance.Dequeue());
+        }
+    }
+}
