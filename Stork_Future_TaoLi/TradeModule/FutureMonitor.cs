4212d7e433f4262283ce44bc86727374269ff505
cheng <skcncspeaker@hotmail.com>
2015-08-17 06:52:00 +0800

    update

---
+++ Stork_Future_TaoLi/TradeModule/FutureMonitor.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Stork_Future_TaoLi;
+using Stork_Future_TaoLi.Queues;
+using System.Threading.Tasks;
+using Stork_Future_TaoLi.Variables_Type;
+using System.Threading;
+using Stork_Future_TaoLi.TradeModule;
+using Stork_Future_TaoLi.Modulars;
+
+namespace Stork_Future_TaoLi
+{
+    public class FutureMonitor
+    {
+
+        private LogWirter log = new LogWirter(); //主线程日志记录
+        private LogWirter sublog = new LogWirter();//子线程日志记录
+
+        public void Main()
+        {
+            //初始化消息队列
+            queue_future_excuteThread.Init();
+
+            //创建线程任务
+            Task FutureTradeControl = new Task(ThreadProc);
+
+            //启动主线程
+            FutureTradeControl.Start();
+
+
+            //日志初始化
+            log.EventSourceName = "期货交易线程控制模块";
+            log.EventLogType = System.Diagnostics.EventLogEntryType.Information;
+            log.EventLogID = 62308;
+
+            sublog.EventSourceName = "期货交易线程模块";
+            sublog.EventLogType = System.Diagnostics.EventLogEntryType.Information;
+            sublog.EventLogID = 62309;
+        }
+
+        private void ThreadProc() {
+
+            //初始化子线程
+            int futureNum = CONFIG.FUTURE_TRADE_THREAD_NUM;
+
+            DateTime lastHeartBeat = DateTime.Now;//本线程最后发送心跳时间
+
+            List<Task> TradeThreads = new List<Task>();
+            log.LogEvent("股票交易控制子线程启动： 初始化交易线程数 :" + futureNum.ToString());
+
+            //启动心跳和交易线程
+            Task.Factory.StartNew(() => HeartBeatThreadProc((object)futureNum));
+            for (int i = 0; i < futureNum; i++)
+            {
+                TradeParaPackage tpp = new TradeParaPackage();
+                tpp._threadNo = (i);
+                object para = (object)tpp;
+                FutureTrade trade = new FutureTrade();
+                trade.SetLog(sublog);
+                TradeThreads.Add(Task.Factory.StartNew(() => trade.FutureTradeSubThreadProc(para)));
+            }
+
+            //此时按照配置，共初始化CONFIG.FUTURE_TRADE_THREAD_NUM 数量交易线程
+            // 交易线程按照方法 FutureTradeSubThreadProc 执行
+
+            //Loop 完成对于子线程的监控
+            //每一秒钟执行一次自检
+            //自检包含任务：
+            //  1. 对每个线程，判断当前交易执行时间，超过 CONFIG.FUTURE_TRADE_OVERTIME 仍未收到返回将会按照 该参数备注执行
+            //  2. 判断当前线程空闲状态，整理可用线程列表
+            //  3. 若当前存在可用线程，同时消息队列（queue_prdTrade_FutureTradeMonitor)
+            //      也包含新的消息送达，则安排线程处理交易
+            //  4. 记录每个交易线程目前处理的交易内容，并写入数据库
+            while (true)
+            {
+                Thread.Sleep(10);
+
+                if ((DateTime.Now - lastHeartBeat).TotalSeconds > 10)
+                {
+                    log.LogEvent("本模块供血不足，线程即将死亡");
+                    break;
+                }
+
+                //获取下一笔交易
+                List<TradeOrderStruct> next_trade = new List<TradeOrderStruct>();
+
+                bool b_get = false;
+
+                if (QUEUE_FUTURE_TRADE.GetQueueNumber() > 0)
+                {
+                    lock (QUEUE_FUTURE_TRADE.GetQueue().SyncRoot)
+                    {
+                        if (QUEUE_FUTURE_TRADE.GetQueue().Count > 0)
+                        {
+                            next_trade = (List<TradeOrderStruct>)QUEUE_FUTURE_TRADE.GetQueue().Dequeue();
+                            b_get = true;
+                        }
+
+                        if (next_trade.Count > 0) { log.LogEvent("期货交易所出队交易数量：" + next_trade.Count.ToString()); }
+                    }
+                }
+
+                if (b_get == true)
+                {
+                    //说明是心跳包    
+                    lastHeartBeat = DateTime.Now;   
+                }
+
+                if (next_trade.Count == 0) continue;
+
+                //此时next_trade中包含了交易参数
+                //判断空闲的线程
+                //利用随机选择，保证线程的平均使用
+
+                Random ran = new Random();
+                bool _bSearch = true;
+                int _tNo = 0;
+                while (_bSearch)
+                {
+
+                    _tNo = ran.Next(0, futureNum);
+                    if (queue_future_excuteThread.GetThreadIsAvailiable(_tNo)) { _bSearch = false; }
+                }
+
+                log.LogEvent("安排线程 ： " + _tNo + " 执行交易 数量： " + next_trade.Count);
+
+                //选择第 _tNo 个线程执行交易
+                queue_future_excuteThread.GetQueue(_tNo).Enqueue((object)next_trade);
+                queue_future_excuteThread.SetThreadBusy(_tNo);
+
+
+                //************************************
+                //将交易发送到相应执行线程后需要做的事情
+                //************************************
+                //if (DateTime.Now.Second % 3 == 0)
+                //{
+                //    //每3秒更新一次线程使用状况
+                //    Console.WriteLine(DateTime.Now.ToString());
+                //    Console.WriteLine("Thread Busy Rate : " + queue_stock_excuteThread.GetBusyNum().ToString() + "/" + stockNum.ToString());
+                //}
+            }
+            Thread.CurrentThread.Abort();
+        }
+
+        private  void HeartBeatThreadProc(object para)
+        {
+            int threadCount = (int)para;
+
+            DateTime lastSubHeaartBeat = DateTime.Now; //子线程最后发送心跳时间
+            DateTime lastHeartBeat = DateTime.Now;//本线程的最后接收心跳时间
+
+            while (true)
+            {
+                Thread.Sleep(10);
+                 
+                if((DateTime.Now - lastHeartBeat).TotalSeconds > 10)
+                {
+                    log.LogEvent("心跳线程即将退出");
+                    break;
+                }
+
+                //向子线程发送存活心跳，一旦心跳停止，则子线程死亡
+                if(DateTime.Now.Second %5 == 0 && DateTime.Now.Second != lastSubHeaartBeat.Second)
+                {
+                    for(int i = 0;i<threadCount;i++)
+                    {
+                        //如果发送空列表，则为心跳线程
+                        List<TradeOrderStruct> o = new List<TradeOrderStruct>();
+                        queue_future_excuteThread.GetQueue(i).Enqueue((object)o);
+                    }
+                }
+
+                lastHeartBeat = GlobalHeartBeat.GetGlobalTime();
+            }
+
+            Thread.CurrentThread.Abort();
+        }
+
+       
+    }
+}
---
+++ Stork_Future_TaoLi/TradeModule/FutureTrade.cs
@@ -0,0 +1,123 @@
+﻿using Stork_Future_TaoLi.TradeModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace Stork_Future_TaoLi
+{
+    public class FutureTrade
+    {
+        private LogWirter sublog = new LogWirter();//子线程日志记录
+        private CTP_CLI.CCTPClient _client;
+
+        private string BROKER = "0292";
+        private string INVESTOR = "";
+        private string PASSWORD = "";
+        private string ADDRESS = "tcp://222.240.130.22:41205";
+
+        private bool ConnectState = false;
+
+        /// <summary>
+        /// 期货交易工作线程
+        /// </summary>
+        /// <param name="para"></param>
+        public void FutureTradeSubThreadProc(object para)
+        {
+
+            string ErrorMsg = string.Empty;
+
+            DateTime lastHeartBeat = DateTime.Now; //最近心跳时间
+
+            //令该线程为前台线程
+            Thread.CurrentThread.IsBackground = true;
+
+            TradeParaPackage _tpp = (TradeParaPackage)para;
+
+            //当前线程编号
+            int _threadNo = _tpp._threadNo;
+
+            sublog.LogEvent("线程 ：" + _threadNo.ToString() + " 开始执行");
+
+            //用作发送心跳包的时间标记
+            DateTime _markedTime = DateTime.Now;
+
+            _client.Connect();
+        }
+
+        /// <summary>
+        /// 设置日志
+        /// </summary>
+        /// <param name="log"></param>
+        public void SetLog(LogWirter log) { this.sublog = log; }
+
+
+        public FutureTrade()
+        {
+            _client = new CTP_CLI.CCTPClient(INVESTOR, PASSWORD, BROKER, ADDRESS);
+            _client.FrontConnected += _client_FrontConnected;
+            _client.FrontDisconnected += _client_FrontDisconnected;
+            _client.RspUserLogin += _client_RspUserLogin;
+            _client.RspError += _client_RspError;
+            _client.RtnOrder += _client_RtnOrder;
+            _client.RtnTrade += _client_RtnTrade;
+            _client.RspOrderAction += _client_RspOrderAction;
+            _client.RspOrderInsert += _client_RspOrderInsert;
+          
+        }
+
+        /// <summary>
+        /// 连接成功
+        /// </summary>
+        void _client_FrontConnected()
+        {
+            //throw new NotImplementedException();
+            this.ConnectState = true;
+        }
+
+        /// <summary>
+        /// 链接失败，记入日志
+        /// </summary>
+        /// <param name="nReason"></param>
+        void _client_FrontDisconnected(int nReason)
+        {
+            sublog.LogEvent("期货交易所链接失败，失败码：" + nReason.ToString());
+            this.ConnectState = false;
+            //throw new NotImplementedException();
+        }
+
+        void _client_RspOrderInsert(CTP_CLI.CThostFtdcInputOrderField_M pInputOrder, CTP_CLI.CThostFtdcRspInfoField_M pRspInfo, int nRequestID, bool bIsLast)
+        {
+                throw new NotImplementedException();
+        }
+
+        void _client_RspOrderAction(CTP_CLI.CThostFtdcInputOrderActionField_M pInputOrderAction, CTP_CLI.CThostFtdcRspInfoField_M pRspInfo, int nRequestID, bool bIsLast)
+        {
+            throw new NotImplementedException();
+        }
+
+        void _client_RtnTrade(CTP_CLI.CThostFtdcTradeField_M pTrade)
+        {
+            throw new NotImplementedException();
+        }
+
+        void _client_RtnOrder(CTP_CLI.CThostFtdcOrderField_M pOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        
+
+        void _client_RspUserLogin(CTP_CLI.CThostFtdcRspUserLoginField_M pRspUserLogin, CTP_CLI.CThostFtdcRspInfoField_M pRspInfo, int nRequestID, bool bIsLast)
+        {
+            throw new NotImplementedException();
+        }
+
+        void _client_RspError(CTP_CLI.CThostFtdcRspInfoField_M pRspInfo, int nRequestID, bool bIsLast)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
