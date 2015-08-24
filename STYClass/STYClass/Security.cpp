6c036dadb03409b18e686ff68525ce69a229694a
cheng <skcncspeaker@hotmail.com>
2015-08-24 11:47:43 +0800

    update

--- STYClass/STYClass/Security.cpp
+++ STYClass/STYClass/Security.cpp
@@ -12,8 +12,8 @@ CSecurity::~CSecurity(void)
 
 bool  CSecurity::setcode(char * temp)
 {
-	strcpy(this->m_DepthMarketData.msecurity.cSecurity_code,temp);
-	 return true;
+	strcpy(this->m_DepthMarketData.msecurity.cSecurity_code, temp);
+	return true;
 }
 
 double   CSecurity::getlastprice()
@@ -23,16 +23,16 @@ double   CSecurity::getlastprice()
 
 bool   CSecurity::isstoped()
 {
-	return this->m_DepthMarketData.marketinfor.bStoped ;
- }
+	return this->m_DepthMarketData.marketinfor.bStoped;
+}
 bool   CSecurity::isupdated()
 {
 	//WaitForSingleObject( hlocalDataMutex, INFINITE ); 	
-	int nCurrentTime =  CTimeUtil::getIntTime();
+	int nCurrentTime = CTimeUtil::getIntTime();
 	if (CTimeUtil::getDeltaSecond(nCurrentTime, this->m_DepthMarketData.marketinfor.LastUpdateTime / 1000)>5 || this->m_DepthMarketData.marketinfor.nInfotLag>5)
-	    this->m_DepthMarketData.bupdated=false;
+		this->m_DepthMarketData.bupdated = false;
 	else
-		this->m_DepthMarketData.bupdated=true;
+		this->m_DepthMarketData.bupdated = true;
 	//ReleaseMutex( hlocalDataMutex );
 	return this->m_DepthMarketData.bupdated;
 }
@@ -42,90 +42,98 @@ double  CSecurity::getrealmarketvalue(int namount)
 }
 double  CSecurity::getrealbuycost(int namount)
 {
-		
-	if(this->m_DepthMarketData.marketinfor.bStoped)
+
+	if (this->m_DepthMarketData.marketinfor.bStoped)
 		return getrealmarketvalue(namount);
-	if(!this->m_DepthMarketData.bupdated)//没有更新
+	if (!this->m_DepthMarketData.bupdated)//没有更新
 		return getrealmarketvalue(namount)*1.02;
-	if(this->m_DepthMarketData.marketinfor.dLastPrice==this->m_DepthMarketData.marketinfor.dHighLimited) //涨停冲击为0
+	if (this->m_DepthMarketData.marketinfor.dLastPrice == this->m_DepthMarketData.marketinfor.dHighLimited) //涨停冲击为0
 		return getrealmarketvalue(namount);
 	int nQtyLeft = namount;
-	if(namount==0)
+	if (namount <= 0)
 		return 0;
 	double cost = 0;
-	int i =0;
+	int i = 0;
 	double dAskVol;
 	double dAskPrice;
 	while (nQtyLeft>0)
 	{
 		dAskPrice = this->m_DepthMarketData.marketinfor.dAskPrice[i];
 		dAskVol = this->m_DepthMarketData.marketinfor.dAskVol[i];
-		if(dAskPrice ==0){ 
-			cost +=nQtyLeft * this->m_DepthMarketData.marketinfor.dLastPrice;
+		if (dAskPrice == this->m_DepthMarketData.marketinfor.dHighLimited)  //为涨停价时， 则结束计算冲击  默认之后冲击为0
+		{
+			cost += nQtyLeft * this->m_DepthMarketData.marketinfor.dLastPrice;
 			return cost;
 		}
-		if(dAskVol >= nQtyLeft){
-			cost +=nQtyLeft * dAskPrice;
+		if (dAskVol >= nQtyLeft)
+		{
+			cost += nQtyLeft * dAskPrice;
 			return cost;
-		}else{
-			cost +=dAskVol * dAskPrice;
-			nQtyLeft -=dAskVol;
+		}
+		else
+		{
+			cost += dAskVol * dAskPrice;
+			nQtyLeft -= dAskVol;
 			i++;
 		}
-		
-		if(i==this->m_DepthMarketData.nMarketdepth)
+
+		if (i == this->m_DepthMarketData.nMarketdepth) //超过行情深度时  
 		{
-			cost +=nQtyLeft * (2*this->m_DepthMarketData.marketinfor.dAskPrice[m_DepthMarketData.nMarketdepth-1]-this->m_DepthMarketData.marketinfor.dAskPrice[m_DepthMarketData.nMarketdepth-2]);
-			
+			cost += nQtyLeft * (2 * this->m_DepthMarketData.marketinfor.dAskPrice[m_DepthMarketData.nMarketdepth - 1] - this->m_DepthMarketData.marketinfor.dAskPrice[m_DepthMarketData.nMarketdepth - 2]);
+			return cost;
 		}
-		
+
 	}
 
 	return cost;
 }
 double  CSecurity::getrealsellgain(int namount)
 {
-	
-	if(this->m_DepthMarketData.marketinfor.bStoped)
+
+	if (this->m_DepthMarketData.marketinfor.bStoped)
 		return getrealmarketvalue(namount);
-	if(!this->m_DepthMarketData.bupdated)//没有更新
+	if (!this->m_DepthMarketData.bupdated)//没有更新
 		return getrealmarketvalue(namount)*0.98;
-	if(this->m_DepthMarketData.marketinfor.dLastPrice==this->m_DepthMarketData.marketinfor.dLowLimited) //跌停冲击为0
+	if (this->m_DepthMarketData.marketinfor.dLastPrice == this->m_DepthMarketData.marketinfor.dLowLimited) //跌停冲击为0
 		return getrealmarketvalue(namount);
 	int nQtyLeft = namount;
-	if(namount==0)
+	if (namount <= 0)
 		return 0;
 	double dGain = 0;
-	int i =0;
-	double dBidPrice,dBidVol;
+	int i = 0;
+	double dBidPrice, dBidVol;
 	while (nQtyLeft>0)
 	{
 		dBidPrice = this->m_DepthMarketData.marketinfor.dBidPrice[i];
 		dBidVol = this->m_DepthMarketData.marketinfor.dBidVol[i];
-		if(dBidPrice ==0){
-			dGain +=nQtyLeft * this->m_DepthMarketData.marketinfor.dLastPrice;
+		if (dBidPrice == this->m_DepthMarketData.marketinfor.dLowLimited)
+		{
+			dGain += nQtyLeft * this->m_DepthMarketData.marketinfor.dLastPrice;
 			return dGain;
 		}
-		if(dBidVol >= nQtyLeft){
-			dGain +=nQtyLeft * dBidPrice;
+		if (dBidVol >= nQtyLeft)
+		{
+			dGain += nQtyLeft * dBidPrice;
 			return dGain;
-		}else{
-			dGain +=dBidVol * dBidPrice;
-			nQtyLeft -=dBidVol;
+		}
+		else
+		{
+			dGain += dBidVol * dBidPrice;
+			nQtyLeft -= dBidVol;
 			i++;
 		}
 
-		if(i==this->m_DepthMarketData.nMarketdepth) 
+		if (i == this->m_DepthMarketData.nMarketdepth)
 		{
-			dGain +=nQtyLeft * (2*this->m_DepthMarketData.marketinfor.dBidPrice[this->m_DepthMarketData.nMarketdepth-1]-this->m_DepthMarketData.marketinfor.dBidPrice[this->m_DepthMarketData.nMarketdepth-2]);
-			
+			dGain += nQtyLeft * (2 * this->m_DepthMarketData.marketinfor.dBidPrice[this->m_DepthMarketData.nMarketdepth - 1] - this->m_DepthMarketData.marketinfor.dBidPrice[this->m_DepthMarketData.nMarketdepth - 2]);
+			return dGain;
 		}
 	}
 
 	return dGain;
 }
 void CSecurity::updateInfo(MarketInforStruct * tempmarketinfor)
-{	
+{
 	this->m_DepthMarketData.marketinfor.update(tempmarketinfor);
 
 }
